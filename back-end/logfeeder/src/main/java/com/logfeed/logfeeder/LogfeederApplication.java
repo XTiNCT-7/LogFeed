81f8f9865d9405b700f923e88853cebd2c3e338a
XTiNCT-7 <iamram48@gmail.com>
2023-05-11 10:46:21 +0530

    spring-boot-1

---
+++ back-end/logfeeder/src/main/java/com/logfeed/logfeeder/LogfeederApplication.java
@@ -0,0 +1,13 @@
+package com.logfeed.logfeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogfeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogfeederApplication.class, args);
+	}
+
+}
---
+++ back-end/logfeeder/src/test/java/com/logfeed/logfeeder/LogfeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logfeed.logfeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogfeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
