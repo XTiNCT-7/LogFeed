3579e13ea9ed5143a6aa7b8bd0c22e30939b7510
Ramprakash nadar <65355702+XTiNCT-7@users.noreply.github.com>
2023-05-23 09:46:01 +0530

    Update Home.jsx

--- front-end/Logfeed-rashmi_3/src/Components/Home.jsx
+++ front-end/Logfeed-rashmi_3/src/Components/Home.jsx
@@ -6,9 +6,18 @@ import Log from './LogItem/Log';
 import './home.css';
 
 const Home = () => {
-    const [time, setTime] = useState({"time":0,"system":""});
+    const [time, setTime] = useState({"time":0,"system":"","date":""});
     const handleFilter=(setTimeFilter)=>{
-        setTime({"time": setTimeFilter,"system":setTimeFilter});
+        setTime({"time": setTimeFilter,"system":setTimeFilter,"date":setTimeFilter});
+    }
+    const handleTime=(setTimeFilter)=>{
+        setTime({"time": setTimeFilter,"system":"","date":""});
+    }
+    const handleSystem=(setTimeFilter)=>{
+        setTime({"time": 0,"system":setTimeFilter,"date":""});
+    }
+    const handleDate=(setTimeFilter)=>{
+        setTime({"time": 0,"system":"","date":setTimeFilter});
     }
     return (
         <>
@@ -16,7 +25,7 @@ const Home = () => {
             <div className='mt-4 container-fluid'>
                 <div className='row'>
                     <div className='col-3'>
-                        <Sidebar onFilter={handleFilter}/>
+                        <Sidebar  onFilterTime={handleTime} onFilterSystem={handleSystem}  onFilterDate={handleDate} />
                     </div>
                     <div className='col-9'>
                         <Log time={time}/>
