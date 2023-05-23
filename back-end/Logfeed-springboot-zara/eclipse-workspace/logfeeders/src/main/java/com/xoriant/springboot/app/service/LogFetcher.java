df97c73b2f0d138ddd87918f0b5edcb745ca4915
Ramprakash nadar <65355702+XTiNCT-7@users.noreply.github.com>
2023-05-23 09:54:37 +0530

    Update LogFetcher.java

--- back-end/Logfeed-springboot-zara/eclipse-workspace/logfeeders/src/main/java/com/xoriant/springboot/app/service/LogFetcher.java
+++ back-end/Logfeed-springboot-zara/eclipse-workspace/logfeeders/src/main/java/com/xoriant/springboot/app/service/LogFetcher.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -85,19 +87,28 @@ public class LogFetcher {
         
         String[] logParts = line.split("\\s+", 6);
         if (logParts.length >= 6) {
-            String timestamp = logParts[0]+" "+logParts[1]+" "+logParts[2];
+            String month = logParts[0];
+            String day = logParts[1];
+            String time= logParts[2];
+
+            String timestamp = month+" "+day+" "+time;
             String sysName = logParts[3];
             String level = logParts[4].substring(0, logParts[4].length()-1);
             String message = logParts[5];
 
-            System.out.println(timestamp +sysName+level+message);
-            SimpleDateFormat dateFormat = new SimpleDateFormat("MMM-dd HH:mm:ss");
+            Calendar calender = Calendar.getInstance();
+            int currentYear = calender.get(Calendar.YEAR);
+
+            
+            SimpleDateFormat dateFormat = new SimpleDateFormat("MMM-dd HH:mm:ss",Locale.ENGLISH);
            
             try {
 
-                Date date = dateFormat.parse(timestamp);
+                
+
+                Date date = parseTimeStamp(timestamp);
                 System.out.println("dateeee"+date);
-                // System.out.println("saving filevin database");
+               
                 Log log = new Log(date, sysName,level, message);
                 System.out.println(log);
                 logRepository.save(log);
@@ -114,5 +125,18 @@ public class LogFetcher {
 
     public void stopLog(){
         running = false;
+    }
+
+    private Date parseTimeStamp(String timestamp) throws ParseException{
+        Calendar curYear = Calendar.getInstance();
+        int year = curYear.get(Calendar.YEAR);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd HH:mm:ss");
+        Date parseDate = dateFormat.parse(timestamp);
+        Calendar parsedCalendar = Calendar.getInstance();
+        parsedCalendar.setTime(parseDate);
+        parsedCalendar.set(Calendar.YEAR, year);
+
+        return parsedCalendar.getTime();
     }
 }
